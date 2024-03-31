fb3eba485fa2702cb43d57c187e60e2e7e40d16c
hp <hp@DESKTOP-9GNBKOB>
2024-03-31 21:25:54 +0500

    commitstagged

---
+++ B1WEB/ActionFilters/SessionAuthorizationFilter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace B1WEB.ActionFilters
+{
+    public class SessionAuthorizationFilter : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            // Check if session exists
+            if (context.HttpContext.Session.GetString("UserId") == null)
+            {
+                // Redirect to login page or perform any other action
+                context.Result = new RedirectToActionResult("Login", "Account", null);
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            // Implement if needed
+        }
+    }
+}
